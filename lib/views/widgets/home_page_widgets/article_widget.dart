3d3a30344f09de674bc6b3421baa48c05d560e5d
Mikhail Zharov <mike.zharov91@gmail.com>
2022-07-14 16:57:38 +0300

    Update article_widget.dart

--- lib/views/widgets/home_page_widgets/article_widget.dart
+++ lib/views/widgets/home_page_widgets/article_widget.dart
@@ -10,10 +10,10 @@ class ArticleWidget extends StatelessWidget{
       child: Column(
         children: const [
           Text(
-            'Lorem ipsum dolor sit amet, consectetur incididunt ut labore et dolore magna aliqua...',
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
             maxLines: 3,
             style: TextStyle(
-              fontSize: 22.0,
+              fontSize: 16.0,
               fontWeight: FontWeight.bold,
             ),),
           SizedBox(height: 10.0,),
@@ -21,7 +21,7 @@ class ArticleWidget extends StatelessWidget{
             'Lorem ipsum dolor sit amet, consectetur incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur incididunt ut labore et dolore magna aliqua psum dolor sit amet, consectetur incididunt ut labore et dolore magna aliqua',
             maxLines: 6,
             style: TextStyle(
-              fontSize: 14.0,
+              fontSize: 12.0,
               color: Color(0xFF696974),
             ),),
         ],
